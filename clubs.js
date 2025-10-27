70efd2f5443f899ee8370c4919b13c0a1d79b7b3
Solomon <olurebisolomon@gmail.com>
2025-10-27 11:11:31 -0700

    added images to the folder and updated the clubs.json

--- clubs.js
+++ clubs.js
@@ -7,15 +7,14 @@ async function loadClubs() {
     const container = document.querySelector('#card-container')
 
     clubs.forEach(club => {
-       container.innerHTML += `<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-all">
-            <img src="https://images.unsplash.com/photo-1522204501921-79a3e2e48d4a?w=600" alt="${club.name}"
-                class="w-full h-40 object-cover">
-            <div class="p-4">
+       container.innerHTML += `<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-all hover:scale-103">
+            <img src="${club.image}" alt="${club.name}"
+                class="w-full h-40 object-cover" />
+            <div class="p-4 relative">
                 <h3 class="text-xl font-semibold text-[#800000] mb-2">${club.name}</h3>
                 <p class="text-gray-600 text-sm mb-4">${club.description}</p>
                 <a href="#"
-                    class="inline-block bg-[#800000] text-white px-4 py-2 rounded-lg hover:bg-[#a64b4b] transition">Join
-                    Now</a>
+                    class="inline-block bg-[#800000] sticky bottom-0 text-white px-4 py-2 rounded-lg hover:bg-[#a64b4b] transition cursor-pointer">Learn More</a>
             </div>
         </div>`;
     });
