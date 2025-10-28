eeea3cb72657c84ef48b28c2c6b6d468d48697c2
Solomon <olurebisolomon@gmail.com>
2025-10-28 09:52:11 -0700

    added the learn more functionality using urlseaarchparams

--- clubs.js
+++ clubs.js
@@ -4,24 +4,34 @@ async function loadClubs() {
     const clubs = await response.json();
     console.log(clubs);
 
-    const container = document.querySelector('#card-container')
+    const container = document.querySelector("#card-container");
 
-    clubs.forEach(club => {
-       container.innerHTML += `<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-all hover:scale-103">
-            <img src="${club.image}" alt="${club.name}"
+    clubs.forEach((club) => {
+      container.innerHTML += `<div class="bg-white rounded-xl shadow-md overflow-hidden hover:shadow-lg transition-all hover:scale-103">
+            <img src="${club.image}" alt="${club.name} loading="lazy"
                 class="w-full h-40 object-cover" />
             <div class="p-4 relative">
                 <h3 class="text-xl font-semibold text-[#800000] mb-2">${club.name}</h3>
                 <p class="text-gray-600 text-sm mb-4">${club.description}</p>
-                <a href="#"
-                    class="inline-block bg-[#800000] sticky bottom-0 text-white px-4 py-2 rounded-lg hover:bg-[#a64b4b] transition cursor-pointer">Learn More</a>
+                <a href="jci.html?club=${club.id}">
+                  <button
+                      class="inline-block bg-[#800000] sticky bottom-0 text-white px-4 py-2 rounded-lg hover:bg-[#a64b4b] transition cursor-pointer" onclick="learnMore(${club.id})">Learn More
+                  </button>
+                </a>
+                
             </div>
         </div>`;
     });
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 }
 
+// document.body =""
 
-loadClubs()
+const learnMore = (id) => {
+  // alert('working'xx)
+  console.log(id);
+};
+
+loadClubs();
